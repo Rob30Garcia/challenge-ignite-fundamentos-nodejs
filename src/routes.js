8da2f02ad4b200d65d1d11aa73134954bc6e5378
Robert Garcia <rgs2@icomp.ufam.edu.br>
2023-02-12 21:09:43 -0400

    feat: create post route and json middleware

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,31 @@
+import { randomUUID } from 'node:crypto';
+
+const tasks = [];
+
 export const routes = [
   {
     method: 'GET',
     path: '/tasks',
     handler: (req, res) => {
-      return res.end('Create Project');
+      return res.writeHead(200).end(JSON.stringify(tasks));
+    }
+  }, 
+  {
+    method: 'POST',
+    path: '/tasks',
+    handler: (req, res) => {
+      const { title, description } = req.body
+
+      tasks.push({
+        id: randomUUID(),
+        title,
+        description,
+        create_at: new Date().toString(),
+        updated_at: new Date().toString(),
+        completed_at: null
+      });
+
+      return res.writeHead(201).end();
     }
   }
 ];
