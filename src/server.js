e59c3c81efa1327703cf72e49935247c6ba32ac3
Robert Garcia <rgs2@icomp.ufam.edu.br>
2023-02-12 20:35:33 -0400

    feat: create routes file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 import http from 'node:http';
+import { routes } from './routes.js';
 
 const server = http.createServer((req, res) =>  {
-  return res.end('Create project');
+  const { method, url } = req;
+
+  const route = routes.find(route => {
+    return route.method === method && route.path === url;
+  });
+
+  if(route) {
+    return route.handler(req, res);
+  }
+
+  return res.writeHead().end();
 })
 
 server.listen(3333);
