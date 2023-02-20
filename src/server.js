9fe1913f8ff3a708dba40a52725bd46b55c3a9bc
Robert Garcia <rgs2@icomp.ufam.edu.br>
2023-02-20 18:47:29 -0400

    chore: filter tasks to search title or description

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js';
+import { extractQueryParams } from './utils/extract-query=params.js';
 
 const server = http.createServer(async (req, res) =>  {
   const { method, url } = req;
@@ -15,14 +16,15 @@ const server = http.createServer(async (req, res) =>  {
     //retornar os dados da regex
     const routeParams = req.url.match(route.path);
 
-    console.log(routeParams);
+    const { query, ...params } = routeParams.groups;
 
-    req.params = {...routeParams.groups};
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
 
     return route.handler(req, res);
   }
 
-  return res.writeHead().end();
+  return res.writeHead(404).end();
 })
 
 server.listen(3333);
